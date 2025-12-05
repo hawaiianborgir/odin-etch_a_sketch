1898cb5c0b25e0973753693a083b3b5a9c2d3d61
hawaiianborgir <shinkyu49@gmail.com>
2025-12-06 00:09:57 +0900

    Create function to create customized grid

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ container.style.width = `800px`;
 container.style.height = `800px`;
 container.style.display = "flex";
 container.style.flexWrap = "wrap";
+
+function pixelCreation(size) {
+    for (i = 1; i <= size*size ; i++) {
+        const pixel = document.createElement("div");
+        pixel.className = "pixel"
+        pixel.style.border = "1px solid #ccc"
+        pixel.style.width = `${800/size}px`
+        pixel.style.height = `${800/size}px`
+        container.appendChild(pixel);
+    }
+}
