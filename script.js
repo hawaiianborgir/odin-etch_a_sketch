35a114a4a2a5c6874c2e2f4236e7e7d9301f9b84
hawaiianborgir <shinkyu49@gmail.com>
2025-12-06 00:08:33 +0900

    Create container for grids

---
+++ script.js
@@ -0,0 +1,5 @@
+const container = document.querySelector(".container");
+container.style.width = `800px`;
+container.style.height = `800px`;
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
